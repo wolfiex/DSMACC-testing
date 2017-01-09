4a1026c87528ebf0601d54809ba8539921c01973
Dan Ellis <daniel.ellis.research@gmail.com>
2017-01-09 18:32:55 +0000

    isporene compiled

---
+++ model_Model.f90
@@ -0,0 +1,20 @@
+MODULE model_Model
+
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!  Completely defines the model model
+!    by using all the associated modules
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  USE model_Precision
+  USE model_Parameters
+  USE model_Global
+  USE model_Function
+  USE model_Integrator
+  USE model_Rates
+  USE model_Jacobian
+  USE model_LinearAlgebra
+  USE model_Monitor
+  USE model_Util
+
+END MODULE model_Model
+
