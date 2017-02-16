3edde681a603af85656095f971926e24ef5177c7
Dan Ellis <daniel.ellis.research@gmail.com>
2017-02-16 17:21:06 +0000

    It CRIs (working) with update to mechanism parser

---
+++ g_isop.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+import netCDF4
+from netCDF4 import Dataset
+import glob,sys,os
+from matplotlib.pyplot import *
+import matplotlib.image as image
+
+myfile = sys.argv[1]
+
+nc = Dataset(myfile,'r')
+print nc.date, '\n', nc.description,'\n'
+
+class spec(object):
+    '''A descriptor for the plot sizes'''
+    def __init__(self,name,ymin,ymax):
+        self.name = name
+        self.ymin = ymin
+        self.ymax = ymax
+
+
+plots = [ spec('O3',40,140),spec('OH',0,1.2e-3),spec('HO2',0,50e-3),spec('HNO3',0,10),spec('C5H8',0,20),spec('HCHO',0,20),spec('MGLYOX',0,2)]
+groups = tuple(nc.groups)
+
+
+#s = shift factor on x axis
+s=-0
+
+for i in plots:
+  try:
+    im = image.imread('gecko-isoprene/%s.png'%i.name)
+    imshow(im, aspect='auto', extent=(0+s, 430+s, i.ymin, i.ymax), zorder=-1, cmap=cm.gray)
+    title(i.name)
+    for g in groups:
+    
+            specs = pd.DataFrame(nc.groups[g].variables['Spec'][:])
+            specs.columns = nc.groups[g].variables['Spec'].head.split(',')
+
+            #rates = pd.DataFrame(nc.groups[g].variables['Rate'][:])
+            #rates.columns = nc.groups[g].variables['Rate'].head.split(',')[:-2]
+
+            
+            a = specs[i.name]/specs.M
+            a = a.map(lambda x : float(x)/1e-9 ) 
+            
+            
+            a.plot(linewidth=2,label = g)
+
+
+
+
+    legend(loc='UL')
+    savefig('gecko-isoprene/mixed_%s.png'%i.name)
+    print 'Saved:', i.name
+  except Exception as e: print e 
+  close()
+    
+    
+# for the nox    
+im = image.imread('gecko-isoprene/NOx.png')
+imshow(im, aspect='auto', extent=(0+s, 430+s, 10e-1, 10e1), zorder=-1, cmap=cm.gray)
+title('NOx')
+for g in groups:
+    
+            specs = pd.DataFrame(nc.groups[g].variables['Spec'][:])
+            specs.columns = nc.groups[g].variables['Spec'].head.split(',')
+
+            #rates = pd.DataFrame(nc.groups[g].variables['Rate'][:])
+            #rates.columns = nc.groups[g].variables['Rate'].head.split(',')[:-2]
+
+            
+            a = specs.NO
+            b = specs.NO2
+            c=a+b
+            c= c/specs.M
+            c = c.map(lambda x : float(x)/1e-9 ) 
+            
+            
+            c.plot(linewidth=2,label = g)
+
+
+
+
+legend(loc='UL')
+savefig('gecko-isoprene/mixed_nox.png')
+
+close()
+print 'Saved:', 'nox'
+
+
+
+
+# for the PANS    
+im = image.imread('gecko-isoprene/PANs.png')
+imshow(im, aspect='auto', extent=(0+s, 430+s, 0, 7), zorder=-1, cmap=cm.gray)
+title('PANs')
+for g in groups:
+    
+            specs = pd.DataFrame(nc.groups[g].variables['Spec'][:])
+            specs.columns = nc.groups[g].variables['Spec'].head.split(',')
+
+            #rates = pd.DataFrame(nc.groups[g].variables['Rate'][:])
+            #rates.columns = nc.groups[g].variables['Rate'].head.split(',')[:-2]
+
+            first = True
+            for i in specs.columns: 
+                if 'PAN' in i:
+                    if first:
+                        c = specs[i]
+                        first =False
+                    else: 
+                        c = c+specs[i]
+                
+            c= c/specs.M
+            c = c.map(lambda x : float(x)/1e-9 ) 
+            
+            
+            c.plot(linewidth=2,label = g)
+
+
+
+
+legend(loc='UL')
+savefig('gecko-isoprene/mixed_PANs.png')
+print 'Saved:', 'pan'
+
+close()
+
+
+    
+nc.close()
---
+++ mechanisms/MCM_subset.py
@@ -0,0 +1,190 @@
+#!/usr/bin/python
+''' 
+A program to extract relevant data regarding the MCM, 
+to be used for extracting subsets
+
+D.Ellis 2016
+'''
+
+import pandas as pd
+import numpy as np
+import os, sys, multiprocessing,re 
+
+available_cores = 16
+
+try: filename=sys.argv[1]
+except:filename1 = 'organic_cri5.kpp'
+full = tuple(open(filename1))
+try: filename=sys.argv[2]
+except:filename = 'inorganic_mcm.kpp'
+inorganics = tuple(open(filename))
+
+
+gen = xrange(len(full))
+
+
+
+''' Step 1 extract all species '''
+inorganic_species =  set(re.findall(r'([A-z0-9]*)[\s=]*IGNORE' ,str(inorganics)))
+all_species =  re.findall(r'([A-z0-9]*)[\s=]*IGNORE' ,str(full))
+sarr = pd.Series(index=all_species)
+sarr[:]=-1
+
+#dictionarys for reference
+num2spec = dict(enumerate(all_species))
+spec2num = {v: k for k, v in num2spec.iteritems()}
+
+
+''' Step 2 split array into reactants and products '''
+fullstr=''.join(full).replace('\n','').replace('\t','').replace(' ','')
+eqn = [i.replace(' ','').split(':') for i in re.findall(r'}([A-z0-9+-=:()*/]*);' ,fullstr)]
+equations = [ i[0].split('=') for i in eqn]
+
+ 
+''' Step 3 split into arrays of each reactant / product '''
+eq_split = [[set(i[0].split('+')), set(i[1].split('+'))] for i in equations]
+ 
+
+gen = xrange(len(equations))
+
+
+#make into a class with run number
+''' get all species formed ''' 
+origin = {'O3','NO','NO2','C5H8'}
+species = origin | inorganic_species ^ set (['' ])
+counter = 0 
+previous = '' 
+
+
+for i in species: 
+    try:        
+        if sarr[i] < 0 :
+            print i
+            sarr[spec2num[i]]= 0
+    except: print i      
+
+while True: 
+    counter += 1 
+    print counter
+
+    dummy = []
+    for i in gen:
+        if eq_split[i][0].issubset(species): 
+            dummy.extend(eq_split[i][1])
+
+    species = species | set(dummy)
+    
+    
+    for i in dummy: 
+        if sarr[i] < 0 :
+            print i
+            sarr[spec2num[i]]= counter
+
+    dummy = []
+
+    if previous == species: break
+    previous = species
+
+
+
+reactions = []
+
+for i in gen:
+    if (eq_split[i][0].issubset(species)) and (eq_split[i][1].issubset(species)): reactions.append(i)
+    
+
+
+### using all reactions generated from specie list 
+##  if a species is in the products, and all reactants present...
+#   this is a generation reaction
+
+
+
+def trace( spec ):
+    created = []
+    loss = []
+    for i in reactions: 
+        if  spec in eq_split[i][1]: created.append(eqn[i] )
+        elif spec in eq_split[i][0]:   loss.append(eqn[i] )
+    return [created,loss]
+
+ 
+s = sarr[sarr>-1]
+xpos = np.linspace(0,1,s.max()+1)
+
+
+
+
+
+traces = multiprocessing.Pool(4).map( trace , s.index ) 
+
+species = species^set(['EMISS'])
+
+
+
+dummy = False
+ro2 = ''
+for i in full: 
+    if 'RO2 = &' in i: dummy = True
+    if 'CALL' in i: break
+    if dummy: ro2+= i 
+    
+ro2 = re.findall('C\(ind_[A-z0-9]*\)',ro2)
+ro2 = [y for y in ro2 if re.search('_([A-z0-9]*)\)',y).group(1) in species]
+
+
+
+
+string = '''// parsed by MCMsubset.py daniel.ellis.research@googlemail.com
+// origin organics: %s
+// %s species  %s reactions
+#DEFFIX
+EMISS=IGNORE;
+''' %(origin,len(species),len(reactions))
+
+
+string += '''
+#INLINE F90_GLOBAL
+REAL(dp)::M, N2, O2, RO2, H2O
+#ENDINLINE
+#INLINE F90_RCONST
+#ENDINLINE 
+#INCLUDE atoms
+#DEFVAR
+HNO3=IGNORE;
+SO2=IGNORE;
+SO3=IGNORE;
+NO3=IGNORE;'''
+
+for i in species:
+    if i == '': continue#i = 'DUMMY'
+    string += i+'=IGNORE;\n'
+    
+string +='''#INLINE F90_RCONST
+USE constants
+RO2 = 0'''
+
+
+for i in ro2: 
+    string += '''&
++%s'''%i 
+
+string += '''
+CALL mcm_constants(time, temp, M, N2, O2, RO2, H2O)
+#ENDINLINE
+'''
+
+
+dummy = False
+for i in inorganics:
+    if '#EQUATIONS' in i: dummy = True
+    if dummy:
+        string += i 
+
+for i in reactions:
+    j = eqn[i]
+    string += '<%04d> %s : %s;\n'%(i,j[0],j[1]) 
+
+
+with open("subset_"+filename1, 'w') as f:
+    f.write(string)
