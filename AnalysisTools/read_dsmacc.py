ca962ea64b0ee1c28cde24abb4dd5dc8305e99da
Dan Ellis <daniel.ellis.research@gmail.com>
2017-05-23 17:41:03 +0100

    current

--- AnalysisTools/read_dsmacc.py
+++ AnalysisTools/read_dsmacc.py
@@ -5,6 +5,7 @@ from netCDF4 import Dataset
 import glob,sys,os
 
 file_list = glob.glob('*.nc')
+file_list.sort(key=os.path.getctime)#getmtime - modified getctime-created
 
 print 'Select file to open: \n\n'
 for i,f in enumerate(file_list): print i , ' - ', f
@@ -13,14 +14,22 @@ myfile = file_list[int(input('Enter Number \n'))]
 nc = Dataset(myfile,'r')
 print nc.date, '\n', nc.description,'\n'
 
-print 'Select Simulation: \n\n'
-for i,g in enumerate(nc.groups): print i , ' - ', g
-group = tuple(nc.groups)[int(input('Enter Number \n'))]
+
+if len(nc.groups) > 1: 
+    print 'Select Simulation: \n\n'
+    for i,g in enumerate(nc.groups): print i , ' - ', g
+    group = tuple(nc.groups)[int(input('Enter Number \n'))]
+else:
+    group = list(nc.groups)[0]
+
 
 print group, 'took', nc.groups[group].WALL_time, 'seconds to compute.'
 
 specs = pd.DataFrame(nc.groups[group].variables['Spec'][:])
 specs.columns = nc.groups[group].variables['Spec'].head.split(',')
+specs.index = pd.to_datetime(specs.TIME, unit='s') 
+
+specs = specs/(specs.M.mean())
 
 rates = pd.DataFrame(nc.groups[group].variables['Rate'][:])
 rates.columns = nc.groups[group].variables['Rate'].head.split(',')[:]
@@ -31,7 +40,32 @@ print 'Spec and Rate files loaded'
 
 from matplotlib.pyplot import *
 
-nc.close()
+
+    
+M= specs['M'].mean()
+
+   
+specs = specs.iloc[1:-1]#remove nan row
+
+
+def compare(spc,skip=0):
+    for group in nc.groups:
+            
+        specs = pd.DataFrame(nc.groups[group].variables['Spec'][:])
+        specs.columns = nc.groups[group].variables['Spec'].head.split(',')
+        specs.index = pd.to_datetime(specs.TIME, unit='s') 
+
+        M= specs['M'].mean()
+        specs = specs/M
+        
+        specs = specs.iloc[1:-1]#remove nan row
+        specs[spc].ix[skip:].plot(label=group)
+    title=spc
+    legend()
+    show()
+
+
+#nc.close()
 
 # Format x-axis for plots
 time = (specs['TIME']-specs['TIME'][0]+600.)/3600.
